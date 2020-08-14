289156ea4f7e1e6c45da2641f3847851d65f6e93
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-08-14 15:45:48 +0900

    创建了<icon />

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    ｜
-    <router-link to="/labels">标签</router-link>
-    ｜
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+import Icon from '@/components/Icon.vue';
 export default {
-  name: 'Nav'
+  name: 'Nav',
+  components: {Icon}
 };
 </script>
 
 <style lang="scss" scoped>
-.nav {border: 1px solid red;}
+.nav {
+  border: 1px solid red;
+}
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(dir).end() // 包含 icons 目录
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
+
+  }
 }
