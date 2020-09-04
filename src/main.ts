c65fe71c29e6e49b255d04da4aae94fb6567fe57
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-09-04 21:52:56 +0900

    尝试引入数字键盘

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import moment from 'moment';
 import 'moment/locale/zh-cn'
 moment.locale('zh-cn');
 
+
 Vue.use(Antd);
 
 Vue.config.productionTip = false
--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -1,19 +1,22 @@
 <template >
   <Layout>
     <Tabs slot="header" class-prefix="interval" :data-source="array1" :value.sync="interval"/>
-    <div slot="body">我来自Reports</div>
+    <div slot="body">
+      <NumericInput placeholder="touch to input" v-model="amount" />
+    </div>
   </Layout>
 </template>
 
 <script lang="ts">
 
+import {NumericInput} from 'numeric-keyboard';
 import Tabs from '@/components/Money/Tabs.vue';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 
 @Component({
-  components: {Tabs}})
+  components: {Tabs,NumericInput}})
 export default class Reports extends Vue{
   array1 = [{text:'月度报告',value:'monthly'},{text:'年度报告',value:'yearly'}]
   interval='monthly'
