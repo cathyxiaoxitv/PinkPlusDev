749f90b06f77f66410fefdbace8e1e61737d62bc
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-09-04 13:49:17 +0900

    删除Parts样式对记账页面的影响

--- src/components/Money/Parts.vue
+++ src/components/Money/Parts.vue
@@ -19,35 +19,5 @@ export default {
 
 <style lang="scss" scoped>
 
-.parts {
-  padding: 5px;
-  display: flex;
-  border-bottom: 1px solid lightgray;
-  min-height: 50px;
-  font-weight: bold;
-  align-items: center;
-  justify-content: space-between;
-
-  p {
-    padding: 10px;
-  }
-
-  .content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-width: 350px;
-    min-height: 40px;
-    padding: 0 10px;
-    margin: 0 30px;
-    border-radius: 5px;
-
-    input{
-      width: 300px;
-      height: 40px;
-    }
-  }
-
-}
 
 </style>
