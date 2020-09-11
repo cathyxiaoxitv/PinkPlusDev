33c0fd9e7825e8e83c840168b5c591f0f7ddb050
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-09-11 21:28:23 +0900

    聚焦清空默认值，离开恢复

--- src/components/Money/Number.vue
+++ src/components/Money/Number.vue
@@ -2,8 +2,10 @@
   <Parts>
     <p slot="title" v-if="type === '-'">支出</p>
     <p slot="title" v-else>收入</p>
-    <label slot="content" >
-      <a-input type="number" pattern="\d*" value="0" prefix="￥" suffix="JPY"/>
+    <label slot="content">
+      <a-input type="number" pattern="\d*" value="0" prefix="￥" suffix="JPY"
+               onfocus="if(value==='0'){value=''}"
+               onblur="if(value===''){value='0'}"/>
     </label>
   </Parts>
 </template>
@@ -11,28 +13,30 @@
 <script lang="ts">
 import Vue from 'vue';
 import Parts from '@/components/Money/Parts.vue';
-import {Component,Prop} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 import Icon from '@/components/Icon.vue';
 
-@Component({components:{Icon, Parts}})
-export default class Number extends Vue{
-  @Prop() type!:string
-  @Prop() readonly  value!:number
+@Component({components: {Icon, Parts}})
+export default class Number extends Vue {
+  @Prop() type!: string
+  @Prop() readonly value!: number
 
   output = this.value.toString();
-  get recordTypeList(){
+
+  get recordTypeList() {
     return this.$store.state.recordTypeList;
   }
-  onValueChanged(event:InputEvent){
+
+  onValueChanged(event: InputEvent) {
     this.output = event.currentTarget.value;
-    this.$emit('update:value',this.output)
+    this.$emit('update:value', this.output)
   }
 
 };
 </script>
 
 <style lang="scss" scoped>
-.ant-input{
+.ant-input {
   background: #FEF0EB;
   font-weight: bold;
   font-size: large;
