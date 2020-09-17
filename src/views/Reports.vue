d1793d7df4c405ade3bc2d6e98555338a603ff2f
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-09-17 18:46:01 +0900

    JS禁止缩放

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -7,13 +7,14 @@
           <li v-for="(group,index) in result" :key="index">
             <h3 class="title">{{ group.title}}</h3>
             <ol>
-
               <li v-for="item in group.item" :key="item.id" class="record">
                 <div class="icon-wrapper">
-                  <Icon :name="item.category.name"></Icon>
-                  {{item.category.name}}</div>
-                <div class="money-wrapper">
-                  {{item.type}}
+                  <Icon :name="item.category.name" class="icon"></Icon>
+                  <span class="tag">{{item.category.name}}</span>
+                  <div class="notes">({{item.notes}})</div>
+                </div>
+                <div class="money-wrapper"
+                     :class="{plus:item.type === '+'}">
                   {{addComma(item.amount.toString())}}
                 </div>
               </li>
@@ -37,6 +38,7 @@ import Tabs from '@/components/Money/Tabs.vue';
   components: {Tabs}
 })
 export default class Reports extends Vue {
+
   array1 = [{text: '月度报告', value: 'monthly'}, {text: '年度报告', value: 'yearly'}]
   interval = 'monthly'
 
@@ -61,6 +63,7 @@ export default class Reports extends Vue {
     console.log(hashTable);
     return hashTable;
   }
+
   addComma(amount:string){
     return amount.replace(/\D/g, "")
         .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -72,25 +75,54 @@ export default class Reports extends Vue {
 %item{
   padding: 0 10px;
   //border-bottom: 1px solid lightgray;
-
-
 }
+
 .title {
   @extend %item;
   min-height: 20px;
   background: #F4F5F6;
 }
 .record{
+  border: 1px solid green;
   min-height: 30px;
   @extend %item;
   display: flex;
   justify-content: space-between;
-}
-.money-wrapper{
-  border: 1px solid red;
-}
-.icon-wrapper{
-  border: 1px solid blue;
+  align-items: center;
+  padding: 5px;
+  .money-wrapper{
+    border: 1px solid red;
+    color: red;
+    &.plus{
+      color: green;
+    }
+
+  }
+
+  .icon-wrapper{
+    border: 1px solid blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .icon{
+      margin: 0 5px;
+      border: 1px solid green;
+      width: 30px;
+      height:30px;
+    }
+    .tag{
+      border: 1px solid pink;
+      font-weight: bold;
+    }
+    .notes{
+      margin: 0 5px;
+      color: gray;
+      font-size: x-small;
+      display: flex;
+      flex-wrap: wrap;
+    }
+  }
+
 }
 
 </style>
