26cf72a63148a5e88d44f9a6868c2ffb33d30ebb
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-09-20 22:54:46 +0900

    引入Echarts

---
+++ src/components/Chart.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="wrapper" ref="wrapper"></div>
+</template>
+
+<script lang="ts">
+
+import {Component,Prop,Vue} from 'vue-property-decorator';
+import Tabs from '@/components/Money/Tabs.vue';
+import echarts, {EChartOption} from 'echarts';
+
+@Component({
+  components: {Tabs,Vue}
+})
+export default class Chart extends Vue{
+  @Prop() options?:EChartOption
+mounted(){
+  if(this.options === undefined){
+    return console.error('options 为空')
+  }
+  const chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
+  chart.setOption(this.options)
+}}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+  height: 400px;
+}
+</style>
--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -2,6 +2,7 @@
   <Layout>
     <Tabs slot="header" class-prefix="interval" :data-source="array1" :value.sync="interval"/>
     <div slot="body">
+      <Chart :options="x"/>
     </div>
   </Layout>
 </template>
@@ -12,72 +13,33 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Money/Tabs.vue';
-
+import Chart from '@/components/Chart.vue'
 
 
 @Component({
-  components: {Tabs}
-})
+  components: {Tabs,Chart}})
+
 export default class Reports extends Vue {
 
   array1 = [{text: '月度报告', value: 'monthly'}, {text: '年度报告', value: 'yearly'}]
   interval = 'monthly'
+  get x(){
+    return{
+      xAxis:{
+        type:'category',
+        data:['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+      },
+      yAxis:{
+        type: 'value'
+      },
+      series:[{
+        data:[820,932,901,934,1290,435,1230],
+        type:'line'
+      }],
+      tooltip:{show:true}
+    }
+  }
 
-  beforeCreate() {
-    this.$store.commit('fetchRecords')
-  }
-
-  get recordList() {
-    return (this.$store.state as RootState).recordList;
-  }
-
-  get result() {
-    const {recordList} = this;
-    const hashTable: { [key: string]: HashTableValue } = {};
-    //声明这个hashTable的key是字符串，value是HashTableValue类型
-    for (let i = 0; i < recordList.length; i++) {
-      const date = recordList[i].createdAt!
-      hashTable[date] = hashTable[date] || {title: date, item: []}
-      hashTable[date].item.push(recordList[i])
-    }
-    return hashTable;
-  }
-showTotal(group:HashTableValue){
-    let total = 0
-    let item:RecordItem
-  for(item of group.item){
-    if(item.type ==='-'){
-      total -= item.amount
-    }
-    else{
-      total += item.amount
-    }
-  }
-  if(total>0){
-    return '+'+this.addComma(total)
-  }else if(total<0){
-    return '-'+this.addComma(total)
-  }else{
-    return '0'
-  }
-}
-addComma(amount:number){
-  return amount.toString().replace(/\D/g, "")
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-showNote(item:RecordItem){
-    if(item.notes!==''){
-      return '('+item.notes+')'
-    }
-}
-  showAmount(item:RecordItem){
-    let commaAmount = this.addComma(item.amount)
-    if(item.type === '+'){
-      return '+'+commaAmount
-    }else{
-      return '-'+commaAmount
-    }
-  }
 };
 </script>
 
